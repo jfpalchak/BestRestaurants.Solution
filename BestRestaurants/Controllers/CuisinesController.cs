63769db4d8d1389746ba4fb85954b4ce46e38990
Joey Palchak <joey.palchak@gmail.com>
2023-10-05 11:39:33 -0700

    add Edit & Delete Views + Controller routes for Cuisine
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -41,5 +41,38 @@ namespace BestRestaurants.Controllers
       Cuisine thisCuisine = _db.Cuisines.Include(cuisine => cuisine.Restaurants).FirstOrDefault(cuisine => cuisine.CuisineId == id);
       return View(thisCuisine);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
+  
+    [HttpPost]
+    public ActionResult Edit(Cuisine cuisine)
+    {
+      _db.Cuisines.Update(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      _db.Cuisines.Remove(thisCuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  
   }
+  
+  
+
 }
